9adb1b438599788b5fb58bdc4346867a6ab1b6e5
Andrei Oproiu <andrei.r.oproiu@gmail.com>
2021-07-08 18:20:21 +0100

    criação da pasta Aulas/aula_2021_3_2_B

---
+++ 15776/Aulas/aula_2021_3_2_B/target/debug/aula_2021_3_2_B.d
@@ -0,0 +1,1 @@
+/Users/zerrocdn/Repos/EDA/15776/Aulas/aula_2021_3_2_B/target/debug/aula_2021_3_2_B: /Users/zerrocdn/Repos/EDA/15776/Aulas/aula_2021_3_2_B/src/main.rs
---
+++ 15776/Aulas/aula_2021_3_2_B/target/debug/deps/aula_2021_3_2_B.d
@@ -0,0 +1,5 @@
+/Users/zerrocdn/Repos/EDA/15776/Aulas/aula_2021_3_2_B/target/debug/deps/aula_2021_3_2_B: src/main.rs
+
+/Users/zerrocdn/Repos/EDA/15776/Aulas/aula_2021_3_2_B/target/debug/deps/aula_2021_3_2_B.d: src/main.rs
+
+src/main.rs:
